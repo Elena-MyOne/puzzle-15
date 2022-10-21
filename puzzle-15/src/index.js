236d3890e2c296200342da423c635d872e56c7a8
Elena <myone72@yahoo.com>
2022-10-20 22:12:38 -0400

    init: start puzle-15 task

---
+++ puzzle-15/src/index.js
@@ -0,0 +1,2 @@
+import "./styles/style.scss";
+import { body } from "./models/body";
---
+++ puzzle-15/src/models/body.js
@@ -0,0 +1,5 @@
+export const body = document.body;
+
+export const wrapper = document.createElement("div");
+wrapper.classList.add("wrapper");
+body.append(wrapper);
